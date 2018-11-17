3177a495df0ae1e3600d686d3871f06119c1e9f5
Stefan Kormann <stefan.kormann@kabsi.at>
2018-11-17 15:35:46 +0100

    Fixed some dumb issue

--- src/main/java/com/korlimann/epic_swords/proxy/client/renderer/entity/RenderTerraBladeBeam.java
+++ src/main/java/com/korlimann/epic_swords/proxy/client/renderer/entity/RenderTerraBladeBeam.java
@@ -78,6 +78,6 @@ public class RenderTerraBladeBeam extends Render<EntityTerraBladeBeam> {
      * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
      */
     protected ResourceLocation getEntityTexture(EntityTerraBladeBeam entity) {
-    	return new ResourceLocation(Reference.MOD_ID, "models/block/terra_blade_beam.json");
+    	return texture;
     }
 }
